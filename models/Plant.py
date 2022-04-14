88f7db315314b821f95b4b626768fd813e94faa9
Oussama Fadlaoui <me@oussamafadlaoui.com>
2022-04-14 16:33:22 +0200

    Some naming conventions and changes to behaviour of health

--- models/Plant.py
+++ models/Plant.py
@@ -4,8 +4,19 @@ class Plant():
   name = "A plant"
 
   def __init__(self) -> None:
-      self.initalHealth = 100
-      self.health = self.initalHealth
+      self.initial_health = 100
+      self.watering_level = 100
+      self.sunlight_level = 100
 
-  def isAlive(self):
-    return self.health > 0
+  """
+  Simply check if the health is greater than 0
+  """
+  def is_alive(self):
+    return self.get_health() > 0
+
+  """
+  The health of a plant is based on the wateringLevel
+  and the sunlightLevel.
+  """
+  def get_health(self):
+    return (self.watering_level + self.sunlight_level) / 2
