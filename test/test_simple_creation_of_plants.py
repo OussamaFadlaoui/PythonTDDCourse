b8300841d47195258b460b9322f0ff1ef95ecee0
Oussama Fadlaoui <me@oussamafadlaoui.com>
2022-04-14 16:06:15 +0200

    Red/Green/Refactor: Step 3 (Refactoring var naming improvements)

--- test/test_simple_creation_of_plants.py
+++ test/test_simple_creation_of_plants.py
@@ -5,10 +5,10 @@ from models.AfricanSheepBush import AfricanSheepBush
 
 class TestSimpleCreationOfPlants(unittest.TestCase):
   
-  def test_creating_african_sheepbush(self):
-    newPlant: AfricanSheepBush = AfricanSheepBush()
-    self.assertIsInstance(newPlant, AfricanSheepBush)
-    self.assertTrue(newPlant.isAlive)
+  def test_creating_african_sheepbush(self) -> None:
+    africanSheepBush: AfricanSheepBush = AfricanSheepBush()
+    self.assertIsInstance(africanSheepBush, AfricanSheepBush)
+    self.assertTrue(africanSheepBush.isAlive)
 
 if __name__ == '__main__':
   unittest.main()
