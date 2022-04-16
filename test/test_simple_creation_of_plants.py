1a2a9b953ac9c1d928e0578214aaa72988bad7e6
Oussama Fadlaoui <me@oussamafadlaoui.com>
2022-04-16 12:54:02 +0200

    Add test: creation of plant

--- test/test_simple_creation_of_plants.py
+++ test/test_simple_creation_of_plants.py
@@ -1,6 +1,7 @@
 import unittest
 
 from models.AfricanSheepBush import AfricanSheepBush
+from models.Plant import Plant
 
 
 class TestSimpleCreationOfPlants(unittest.TestCase):
@@ -9,6 +10,11 @@ class TestSimpleCreationOfPlants(unittest.TestCase):
     africanSheepBush: AfricanSheepBush = AfricanSheepBush()
     self.assertIsInstance(africanSheepBush, AfricanSheepBush)
     self.assertTrue(africanSheepBush.is_alive())
+
+  def test_creating_plant(self) -> None:
+    plant: Plant = Plant()
+    self.assertIsInstance(plant, Plant)
+    self.assertTrue(plant.is_alive())
 
 if __name__ == '__main__':
   unittest.main()
