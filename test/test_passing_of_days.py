96c7a9b96470bce4e1b55682777908675000e030
Oussama Fadlaoui <me@oussamafadlaoui.com>
2022-04-16 12:52:26 +0200

    Red test: Passing days deteroriates plant health

---
+++ test/test_passing_of_days.py
@@ -0,0 +1,16 @@
+
+import unittest
+from models.Plant import Plant
+
+
+class TestPassingOfDays(unittest.TestCase):
+
+  def test_passing_of_a_day_deteroriates_plant_health(self) -> None:
+    some_plant: Plant = Plant()
+    initial_health: int = some_plant.get_health()
+
+    some_plant.pass_day()
+
+    health_after_days: int = some_plant.get_health()
+
+    self.assertNotEqual(initial_health, health_after_days)
