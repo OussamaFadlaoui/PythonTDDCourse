f1b389e45e7b21d15d9102637ee02cafec1ed02f
Oussama Fadlaoui <me@oussamafadlaoui.com>
2022-04-18 16:15:21 +0200

    Testing Best Practice: 1 assertion per test

--- test/test_simple_creation_of_plants.py
+++ test/test_simple_creation_of_plants.py
@@ -8,12 +8,10 @@ class TestSimpleCreationOfPlants(unittest.TestCase):
   
   def test_creating_african_sheepbush(self) -> None:
     africanSheepBush: AfricanSheepBush = AfricanSheepBush()
-    self.assertIsInstance(africanSheepBush, AfricanSheepBush)
     self.assertTrue(africanSheepBush.is_alive())
 
   def test_creating_plant(self) -> None:
     plant: Plant = Plant()
-    self.assertIsInstance(plant, Plant)
     self.assertTrue(plant.is_alive())
 
 if __name__ == '__main__':
