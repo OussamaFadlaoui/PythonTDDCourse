39f9167b2b2f8c1e41e56805bc2c0cdfcb4d7c71
Oussama Fadlaoui <me@oussamafadlaoui.com>
2022-04-18 12:56:18 +0200

    Green test: Add plant to garden

--- models/Garden.py
+++ models/Garden.py
@@ -8,3 +8,11 @@ class Garden:
   def __init__(self, capacity: int = 10) -> None:
     self.plants: List[Plant] = list()
     self.capacity: int = capacity
+
+  def add_plant(self, plant_to_add: Plant) -> bool:
+    if len(self.plants) >= self.capacity:
+      return False
+
+    self.plants.append(plant_to_add)
+    
+    return True
