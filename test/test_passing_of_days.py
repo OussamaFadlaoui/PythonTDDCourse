76dce7c1bb65033dd5da295feaa3ab6dc42921c2
Oussama Fadlaoui <me@oussamafadlaoui.com>
2022-04-18 12:50:37 +0200

    Add test description

--- test/test_passing_of_days.py
+++ test/test_passing_of_days.py
@@ -5,6 +5,9 @@ from models.Plant import Plant
 
 class TestPassingOfDays(unittest.TestCase):
 
+  """
+  Days passing should mean that a plant's health deteriorates
+  """
   def test_passing_of_a_day_deteroriates_plant_health(self) -> None:
     some_plant: Plant = Plant()
     initial_health: int = some_plant.get_health()
